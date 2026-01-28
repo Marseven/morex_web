ac7104f16c11fa7753e60247086a13f52cee8950
Aristide Mebodo <mebodoaristide@yahoo.fr>
2026-01-28 16:04:58 +0100

    fix(mail): preserve logo aspect ratio in emails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel' || trim($slot) === config('app.name'))
-<img src="{{ config('app.url') }}/images/logo.png" class="logo" alt="Morex" style="max-height: 50px;">
+<img src="{{ config('app.url') }}/images/logo.png" class="logo" alt="Morex" style="height: 40px; width: auto; max-width: 200px;">
 @else
 {!! $slot !!}
 @endif
